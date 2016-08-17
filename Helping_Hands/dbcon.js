26041f0d4212c1b5aa6e1cec3af69256bc214aba
Will <will.kim89@gmail.com>
2016-08-16 17:43:11 -0700

    Update dbcon.js

--- Helping_Hands/dbcon.js
+++ Helping_Hands/dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   host  : 'localhost',
   user  : 'root',
-  password: 'apple44',
-  database: 'sys' 
+  password: 'password',
+  database: 'system' 
 });
 
 module.exports.pool = pool;
